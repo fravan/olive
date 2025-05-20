9274515b6a5392b9e2610e4c5f22b14481763a90
Alen Sugimoto <alensugimoto@gmail.com>
2025-05-19 17:11:49 -0700

    Add page reload on WebSocket reconnect to prevent stale views

--- priv/proxy.js
+++ priv/proxy.js
@@ -1,12 +1,20 @@
 let liveReloadWebSocket = null;
 let reconnectTimeout = null;
 
+const RELOAD_ON_RECONNECT_KEY = "shouldReload";
+
 function connect() {
   clearTimeout(reconnectTimeout);
   liveReloadWebSocket = new WebSocket(
     `ws://${window.location.host}/ws_livereload`,
   );
 
+  liveReloadWebSocket.onopen = () => {
+    if (sessionStorage.getItem(RELOAD_ON_RECONNECT_KEY)) {
+      sessionStorage.removeItem(RELOAD_ON_RECONNECT_KEY);
+      window.location.reload();
+    }
+  };
   liveReloadWebSocket.onmessage = (event) => {
     window.location.reload();
   };
@@ -15,6 +23,7 @@ function connect() {
 }
 
 function reconnect() {
+  sessionStorage.setItem(RELOAD_ON_RECONNECT_KEY, "true");
   clearTimeout(reconnectTimeout);
   reconnectTimeout = setTimeout(connect, 5000);
 }
